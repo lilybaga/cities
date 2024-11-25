b5843fedbd584c1359b7f37401b9e0328783173d
lilybaga <lilybagar@gmail.com>
2024-11-25 19:24:23 +0300

    Добавлена возможность отображения карты города в браузере при нажатии

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
 from opencage.geocoder import OpenCageGeocode
 from tkinter import *
+import webbrowser
 
 
 def get_coords(city, key):
     try:
         geocoder = OpenCageGeocode(key)
-        results = geocoder.geocode(city, lanuage='ru')
+        results = geocoder.geocode(city, language='ru')
         if results:
             lat=round(results[0]["geometry"]["lat"],2)
             lng=round(results[0]["geometry"]["lng"],2)
             country = results[0]['components']['country']
+            # Получаем URL для OpenStreetMap
+            osm_url = f"https://www.openstreetmap.org/?mlat={lat}&mlon={lng}"
+
             if 'state' in results[0]['components']:
                 region = results[0]['components']['state']
-                return f"Широта: {lat}, Долгота: {lng}\nСтрана: {country}\nРегион: {region}"
+                return {
+                    "coordinates": f"Широта: {lat}, Долгота: {lng}\nСтрана: {country}\nРегион: {region}",
+                        "map_url": osm_url
+                        }
+
             else:
-                return f"Широта: {lat}, Долгота: {lng}\nСтрана: {country}."
+                return {
+                    "coordinates": f"Широта: {lat}, Долгота: {lng}\nСтрана: {country}",
+                        "map_url": osm_url
+                        }
 
         else:
             print("Данные не найдены")
@@ -24,15 +35,26 @@ def get_coords(city, key):
 
 def show_coordinates(event=None):
     city = entry.get()
-    coordinates = get_coords(city, key)
-    label.config(text=coordinates)
+    result = get_coords(city, key)
+    label.config(text=result["coordinates"])
+    # Сохраняем URL в глобальной переменной для доступа из другой функции
+    global map_url
+    map_url = result["map_url"]
+
+
+def show_map():
+    if map_url:
+        webbrowser.open(map_url)
+
 
 key = '4b118bba547640c0b0a5b30c043600f4'
 
 
 window = Tk()
 window.title("Поиск координат города")
-window.geometry("200x100")
+window.geometry("250x160")
+
+map_url = None
 
 # Элементы интерфейса
 entry = Entry()
@@ -44,6 +66,9 @@ button.pack()
 
 label = Label(text="Введите город и нажмите Поиск")
 label.pack()
+
+map_button = Button(text="Показать карту", command=show_map)
+map_button.pack()
 
 # Запуск приложения
 window.mainloop()
